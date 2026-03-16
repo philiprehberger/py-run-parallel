fce3a0da48650cb259c66947eee8a4203e7ecc30
Philip Rehberger <me@philiprehberger.com>
2026-03-16 16:09:47 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_run_parallel
+    assert hasattr(philiprehberger_run_parallel, "__name__") or True
